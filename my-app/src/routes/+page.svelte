b317196af30b5958f866444ca4f5aa459920b7b8
PakiQuian <f.quianblanco@gmail.com>
2025-02-27 13:44:54 +0000

    Supabase connection

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    export let data;
+  </script>
+
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+    <ul>
+        <li>HOLA</li>
+        {#each data.statuses as status}
+          <li>{status.name}</li>
+        {/each}
+        <li>CHAU</li>
+    </ul>
+      
