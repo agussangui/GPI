2aeb2d9d7d7c02793c635e7fb472d322373aaf41
PakiQuian <f.quianblanco@gmail.com>
2025-03-19 01:33:05 +0000

    Favicon + App title

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
   import "tailwindcss/tailwind.css";
   import "$lib/styles/global.css";
   import { onMount } from 'svelte';
@@ -6,13 +6,11 @@
   import { userStore } from '$stores/userStore';
 
   onMount(() => {
-    // Check if user is already logged in by retrieving session
     const checkSession = async () => {
       const { data } = await supabase.auth.getSession();
       console.log(data)
       
       if (data?.session) {
-        // If session exists, update the user store
         userStore.set({
           authUser: data.session.user,
           session: data.session
@@ -22,7 +20,6 @@
     
     checkSession();
 
-    // Set up auth state listener for future auth changes
     const { data: { subscription } } = supabase.auth.onAuthStateChange(
       (_event, session) => {
         userStore.set({
@@ -32,7 +29,6 @@
       }
     );
 
-    // Clean up subscription when component is destroyed
     return () => {
       subscription.unsubscribe();
     };
