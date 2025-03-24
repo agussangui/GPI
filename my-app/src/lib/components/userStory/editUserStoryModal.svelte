8fa800f3a068b62a11c58b4701446df67eda0d52
agussang <msanguinetti@itba.edu.ar>
2025-03-24 01:18:02 +0000

    added ui to edit user stories

---
+++ my-app/src/lib/components/userStory/editUserStoryModal.svelte
@@ -0,0 +1,75 @@
+<script lang="ts">
+	import type {UserStoryInterface} from "$models/userStory";
+    import CloseBtn from "../icons/CloseBtn.svelte";
+    import ErrorToast from "../alerts/errorToast.svelte";
+    import { page } from '$app/state';
+    import { error, type RequestEvent } from '@sveltejs/kit';
+    import { onMount } from "svelte";
+	import { UserStoryStatusEnum } from "$models/userStoryStatusEnum.ts";
+
+
+    export let userStory
+    export let showEditModal: boolean
+      
+    let newUserStory: UserStoryInterface;
+    let errorToaster : boolean = false;
+    let projectId: string;   
+
+    async function submitForm(event: SubmitEvent) {
+        event.preventDefault();
+        const form = event.currentTarget as HTMLFormElement; 
+        const formData = new FormData(form); 
+
+        const jsonData = {
+            title: formData.get('title') as string,
+            description: formData.get('description') || null,
+            priority: Number(formData.get('priority')), 
+            story_points: Number(formData.get('story_points')) || null, 
+        };
+
+        try {
+        const response = await fetch('/api/user_stories', {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(jsonData),
+        });
+
+        console.log(JSON.stringify(jsonData))
+        const result = await response.json();
+        console.log('🔄 Respuesta del servidor:', result);
+        } catch (error) {
+            errorToaster =true
+        console.error('❌ Error en la petición:', error);
+        }
+    }
+
+    onMount(()=>{
+      if (page.params) {
+            projectId = page.params.id;
+        }
+    })
+</script>
+
+<div class="modal" data-theme="light" class:modal-open={showEditModal}>
+    <div class="modal-box text-neutral">
+        <div class="flex items-center justify-between mb-5">
+          <h3 class="font-bold text-lg ">Edit user story</h3> 
+          <div class="" onclick={()=> showEditModal=false}> <CloseBtn/></div>
+        </div>
+      <form id='form' class="fieldset" onsubmit={(e) => { submitForm(e); }}>
+        <input type="text" name="title" class="input" placeholder={userStory.title} />
+        <input type="text" name="description" class="input" placeholder={userStory.description} />
+        <input type="number" name="story_points" class="input validator" required placeholder={ userStory.story_points.toString()}/>
+        <input type="number" name="priority" class="input validator" required placeholder={ userStory.priority.toString()}/>
+        
+      <div class="modal-action">
+              
+        <button class="btn" onclick={()=> showEditModal=false}>Add</button>
+      </div>
+      </form>
+    </div>
+  </div>
+
+  {#if errorToaster}
+    <ErrorToast title=error/>
+  {/if}
--- my-app/src/lib/components/userStory/userStory.svelte
+++ my-app/src/lib/components/userStory/userStory.svelte
@@ -6,12 +6,17 @@
   import { UserStoryStatusEnum } from "$models/userStoryStatusEnum";
   import { get } from 'svelte/store';
   import { sprintStore } from "$stores/sprintStore";
+	import ThreeDots from "../icons/ThreeDots.svelte";
+	import Edit from "../icons/Edit.svelte";
+	import EditUserStoryModal from "./editUserStoryModal.svelte";
   let { userStory, addUserStoryToSprint, addUserStoryToBacklog, removeUserStory } = $props();
 
   let isDeleted: boolean = $state(false);
   const { upcomingSprint } = get(sprintStore);
-  
+  let showEditModal: boolean = $state(false);
 </script>
+
+  <EditUserStoryModal bind:showEditModal userStory={userStory} />  
 
 {#if !isDeleted}
   <li class="list-row flex items-center justify-between px-5 py-4 border-b border-gray-200 hover:bg-gray-50">
@@ -21,26 +26,43 @@
       <div class="text-gray-600">{userStory.story_points} SP</div>
     </div>
     
-    <div class="flex items-center space-x-2">
+    <div class="flex items-center space-x-2 ">
       {#if !userStory.owners?.size}
         <UserIcon/>
       {/if}
       
       {#if userStory.status_id == UserStoryStatusEnum.backlog}
         {#if upcomingSprint}
-          <button class="btn btn-circle btn-ghost p-2 text-gray-600 hover:bg-info" onclick={() => addUserStoryToSprint(userStory)}>
+          <button class="btn btn-circle btn-ghost p-2 w-9 h-9 text-gray-600 hover:bg-info" onclick={() => addUserStoryToSprint(userStory)}>
             <Add/>
           </button>
           {/if}
       {:else}
-        <button class="btn btn-circle btn-ghost p-2 text-gray-600 hover:bg-warning" onclick={() => addUserStoryToBacklog(userStory)}>
+        <button class="btn btn-circle btn-ghost p-2 w-9 h-9 text-gray-600 hover:bg-warning" onclick={() => addUserStoryToBacklog(userStory)}>
           <Minus/>
         </button>
       {/if }
 
-      <button class="btn btn-circle btn-ghost p-2 text-gray-600 hover:bg-danger" onclick={() => removeUserStory(userStory)}>
-        <Delete/>
+      <button popovertarget="popover-{userStory.id}" style="anchor-name:--anchor-{userStory.id}" class="btn btn-circle btn-ghost p-2 w-9 h-9 text-gray-600 hover:bg-danger">
+        <ThreeDots/>
       </button>
+      <ul class="dropdown menu w-52 rounded-box bg-base-100 shadow-sm"
+        popover id="popover-{userStory.id}" style="position-anchor:--anchor-{userStory.id}">
+        <li> 
+          <button onclick={(e) =>{ e.stopPropagation(); showEditModal = true; }} class="flex items-center gap-2">
+            <div class="w-4 h-4"> <Edit /></div>
+            <span>Edit</span>
+          </button> 
+        </li>
+        <li>
+          <span> 
+            <button onclick={(e) => {e.stopPropagation(); removeUserStory(userStory)}} class="flex items-center gap-2">
+              <div class="w-4 h-4"> <Delete /></div>
+              <span>Delete</span>
+            </button> 
+          </span>          
+        </li>
+      </ul>
     </div>
   </li>
 {/if}
